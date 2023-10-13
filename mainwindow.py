69663946cf5e328f8078e0ba4560e1bc58ac6578
TheoChoffat <choffatt@hotmail.com>
2023-10-13 18:33:55 +0200

    Results addidion

--- mainwindow.py
+++ mainwindow.py
@@ -1,32 +1,88 @@
 import tkinter as tk
-from tkinter import filedialog, Entry, Label
-from search import get_layout, search_in_description
+from tkinter import *
+import customtkinter as ctk
+from search import get_layout, insert_newlines, search_name, truncate_text
+from tkinter import font
+
+ctk.set_appearance_mode("light")
+ctk.set_default_color_theme("blue")
 
 class MainWindow(tk.Tk):
     def __init__(self):
         super().__init__()
+        self.geometry("800x600")
         self.database_root = "C:/Users/choff/OneDrive/Bureau/TB/BIDS_Database"  # path to the root directory of all your datasets
-
         self.title("BIDS Search App")
+        self.name_font = font.Font(family='Helvetica', size=10, weight='bold')
+        self.path_font = font.Font(family='Helvetica', size=10)
         
-        # Entry for search keyword
-        self.keyword_entry = Entry(self)
-        self.keyword_entry.pack(pady=20)
+    
+        # TOP FRAME
+        # Top frame for filters
+        self.top_frame = ctk.CTkFrame(self)
+        self.top_frame.pack(fill="x", pady=(20, 10), padx=20)
         
+        # Title for Database Filters
+        self.filter_label = Label(self.top_frame, text="Database Filters :", anchor="w")
+        self.filter_label.pack(pady=10, padx=5, anchor="w")
+
+        # Label and Entry for search keyword
+        self.name_label = Label(self.top_frame, text="Dataset Name: ")
+        self.name_label.pack(side="left", pady=10, padx=5)
+        
+        self.keyword_entry = Entry(self.top_frame)
+        self.keyword_entry.pack(side="left", fill="none", expand=False)
+        
+        
+        
+        
+        # SEARCH BUTTON
         # Button to initiate search
-        self.search_btn = tk.Button(self, text="Search", command=self.search_datasets)
-        self.search_btn.pack(pady=20)
+        self.search_btn = ctk.CTkButton(self, text="Search", command=self.search_datasets)
+        self.search_btn.pack(padx=20, fill="x")
+
+
+
+
+        # BOTTOM RESULTS
+        # Bottom scrollable frame for results
+        self.bottom_frame = ctk.CTkScrollableFrame(self, height=500) 
+        self.bottom_frame.pack(fill="x", pady=(10), padx=20, expand=True)
+
+        # Title for Results
+        self.result_title_label = Label(self.bottom_frame, text="Results :", anchor="w")
+        self.result_title_label.pack(pady=10, padx=5, anchor="w")
 
         # Label to display search results
-        self.result_label = Label(self, text="")
-        self.result_label.pack(pady=20)
+        self.result_label = Label(self.bottom_frame, text="", anchor="w", justify="left")
+        self.result_label.pack(pady=20, padx=5, anchor="w", fill="x")
+        self.result_label.config(text="Welcome, please enter a filter and press Search.")
+
 
     def search_datasets(self):
         keyword = self.keyword_entry.get()
-        matching_datasets = search_in_description(self.database_root, keyword)
+        matching_datasets = search_name(self.database_root, keyword)
 
-        # Display the results in the result_label
+        # Clear previous results
+        for widget in self.bottom_frame.winfo_children():
+            widget.destroy()
+
         if matching_datasets:
-            self.result_label.config(text="\n Dataset Name : ".join(matching_datasets))
+            for name, path, readme_preview in matching_datasets:
+                # Create a Label for the dataset name
+                name_label = Label(self.bottom_frame, text=truncate_text(name, 100), anchor="w", justify="left", foreground="black", font=self.name_font)
+                name_label.pack(pady=(5, 0), padx=5, anchor="w", fill="x")
+
+                # Create a Label for the dataset path
+                path_label = Label(self.bottom_frame, text=path, anchor="w", justify="left", foreground="green", font=self.path_font)
+                path_label.pack(pady=(0, 0), padx=5, anchor="w", fill="x")
+
+                # Create a Label for the README preview
+                readme_label = Label(self.bottom_frame, text=truncate_text(insert_newlines(readme_preview, 125),500), anchor="w", justify="left", font=self.path_font)
+                readme_label.pack(pady=(0, 10), padx=5, anchor="w", fill="x")
         else:
-            self.result_label.config(text="No matching datasets found.")
+            no_result_label = Label(self.bottom_frame, text="No matching datasets found.", anchor="w", justify="left")
+            no_result_label.pack(pady=20, padx=5, anchor="w", fill="x")
+            
+    
+    
--- search.py
+++ search.py
@@ -13,7 +13,17 @@ def get_all_datasets(database_root):
     """ Return a list of all datasets (full paths) in the given database root. """
     return [os.path.join(database_root, d) for d in os.listdir(database_root) if os.path.isdir(os.path.join(database_root, d))]
 
-def search_in_description(database_root, keyword):
+def get_readme_preview(dataset_dir):
+    """ Return the first three non-empty lines of the README file in the dataset directory. """
+    readme_path = os.path.join(dataset_dir, 'README')
+    if os.path.exists(readme_path):
+        with open(readme_path, 'r') as f:
+            lines = [line.strip() for line in f.readlines() if line.strip()]  # Filter out empty lines
+            return '\n'.join(lines[:2])
+    return ""
+
+
+def search_name(database_root, keyword):
     dataset_dirs = get_all_datasets(database_root)
     matching_datasets = []
 
@@ -23,8 +33,35 @@ def search_in_description(database_root, keyword):
             with open(description_file, 'r') as f:
                 data = json.load(f)
                 if keyword in data.get('Name', ''):
-                    matching_datasets.append(data['Name'])
-                if keyword in data.get('Authors', ''):
-                    matching_datasets.append(data['Authors'])
+                    readme_preview = get_readme_preview(dataset_dir)
+                    matching_datasets.append((data['Name'], dataset_dir, readme_preview))
 
     return matching_datasets
+
+def truncate_text(text, max_length):
+        if len(text) > max_length:
+            return text[:max_length-3] + "..."
+        else:
+            return text
+
+def insert_newlines(text, position=1):
+    chunks = []
+    while text:
+        # If the remaining text is shorter than the position, just append it and break
+        if len(text) <= position:
+            chunks.append(text)
+            break
+
+        # Find the first space after the given position
+        space_index = text.find(' ', position)
+
+        # If there's no space after the position, take the entire remaining text
+        if space_index == -1:
+            chunks.append(text)
+            break
+
+        # Split the text at the found space and append the chunk
+        chunks.append(text[:space_index])
+        text = text[space_index+1:]  # Remaining text for next iteration
+
+    return '\n'.join(chunks)
